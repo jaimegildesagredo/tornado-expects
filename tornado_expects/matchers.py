576b4a6298400c907f8243f3f9f66b5a19d94c7c
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2015-11-08 11:26:37 +0100

    Work with expects 0.8.0

--- tornado_expects/matchers.py
+++ tornado_expects/matchers.py
@@ -11,28 +11,22 @@ from expects.matchers.built_in import have_key, have_keys
 
 class _be_ok(Matcher):
     def _match(self, response):
-        return response.code < httplib.BAD_REQUEST
+        return response.code < httplib.BAD_REQUEST, []
 
 
 class _be_json(Matcher):
     def _match(self, response):
-        return 'application/json' in response.headers['Content-Type']
+        return 'application/json' in response.headers['Content-Type'], []
 
 
 class have_header(have_key):
     def _match(self, response):
         return super(have_header, self)._match(response.headers)
 
-    def _description(self, response):
-        return super(have_header, self)._description(response.headers)
-
 
 class have_headers(have_keys):
     def _match(self, response):
         return super(have_headers, self)._match(response.headers)
-
-    def _description(self, response):
-        return super(have_headers, self)._description(response.headers)
 
 
 class have_status(Matcher):
@@ -40,7 +34,7 @@ class have_status(Matcher):
         self._expected = expected
 
     def _match(self, response):
-        return response.code == self._expected
+        return response.code == self._expected, []
 
 be_ok = _be_ok()
 be_json = _be_json()
